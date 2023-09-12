01640b0a0a22fe187d5fb9be33fb96909df42815
MatePC <xklime44@vutbr.cz>
2023-09-12 23:44:49 +0200

    bg bug fix

--- src/Aquarium.vue
+++ src/Aquarium.vue
@@ -7,6 +7,7 @@ const props = defineProps({
 
 <template>
   <div class="aquarium w-full h-3/4 bg-green-100">
+    <img src="bg.jpg" class="absolute w-screen h-screen opacity-25" alt="">
     <Fish
       v-for="fish in fishes"
       :key="fish.url"
@@ -17,7 +18,7 @@ const props = defineProps({
 
 <style scoped>
 .aquarium {
-  background-image: url("bg.jpg");
+  /* background-image: url("bg.jpg"); */
   background-repeat: no-repeat;
   background-position: center;
   background-size: cover;
